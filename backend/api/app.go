21368f0c7c44ce0dd11212e3b15281f19e28571b
Liz <liz.bode20@gmail.com>
2020-09-19 15:03:55 +0200

    Add DB client initialization and connection

--- backend/api/app.go
+++ backend/api/app.go
@@ -1,14 +1,57 @@
 package api
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 )
 
-type App struct{}
+type App struct {
+	Client *Client
+}
+
+type Client struct {
+	Username string
+	Password string
+	Host     string
+	Port     string
+	Database string
+	DB       *sql.DB
+}
 
 func (a *App) Run(addr string) {
 	log.Printf(fmt.Sprintf("Notes API listening on %v...\n", addr[1:]))
 	log.Fatalln(http.ListenAndServe(addr, nil))
+}
+
+func (c *Client) connect() (*sql.DB, error) {
+	params := fmt.Sprintf("postgres://%v:%v@%v:%v/%v?sslmode=disable",
+		c.Username,
+		c.Password,
+		c.Host,
+		c.Port,
+		c.Database,
+	)
+
+	db, err := sql.Open("postgres", params)
+	if err != nil {
+		return nil, err
+	}
+
+	db.SetMaxIdleConns(1)
+	db.SetMaxOpenConns(1)
+	return db, err
+}
+
+func (a *App) Initialize() error {
+	var err error
+	database, err := a.Client.connect()
+	if err != nil {
+		log.Println("Failed to connect to database")
+		return err
+	}
+	a.Client.DB = database
+
+	return nil
 }
--- backend/main.go
+++ backend/main.go
@@ -1,11 +1,27 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"github.com/lizzle-shizzle/notes-application/backend/api"
 )
 
 func main() {
-	a := api.App{}
+	a := api.App{
+		Client: &api.Client{
+			Username: os.Getenv("DB_USERNAME"),
+			Password: os.Getenv("DB_PASSWORD"),
+			Host:     os.Getenv("DB_HOST"),
+			Port:     os.Getenv("DB_PORT"),
+			Database: os.Getenv("DB_DATABASE"),
+		},
+	}
+	err := a.Initialize()
+	if err != nil {
+		log.Fatalf("Failed to initialize service: %s\n", err)
+		os.Exit(1)
+	}
 
 	a.Run(":8080")
 }
