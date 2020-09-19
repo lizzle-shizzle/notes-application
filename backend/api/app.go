58d15dc90b2f31f2e46d2c8400fa56275c6f1fb9
Liz <liz.bode20@gmail.com>
2020-09-19 21:27:41 +0200

    Update endpoint handler to fix CORS issue

--- backend/api/app.go
+++ backend/api/app.go
@@ -76,7 +76,12 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 }
 
 func (a *App) NotesHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 	switch r.Method {
+	case "OPTIONS":
+		return
 	case "GET":
 		notes, err := FetchNotes(a.Client)
 		if err != nil {
