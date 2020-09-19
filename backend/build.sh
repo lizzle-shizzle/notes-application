739520d6544dfc533765e48f920830580c443bd0
Liz <liz.bode20@gmail.com>
2020-09-19 16:01:53 +0200

    Add build script

---
+++ backend/build.sh
@@ -0,0 +1,8 @@
+IMAGE='notes-api'
+
+echo '>>> Building command...'
+CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o app .
+echo '...Done'
+
+echo '>>> Building image...'
+docker image build -t $IMAGE .
