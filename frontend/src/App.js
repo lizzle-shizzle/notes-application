0d9d80d4bfc29c9d42c2b342f4194fc1e6421603
Liz <liz.bode20@gmail.com>
2020-09-20 21:23:10 +0200

    Add basic frontend structure

---
+++ frontend/src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Col, Row } from 'reactstrap';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+        <br></br>
+      </div>
+    );
+  }
+}
+
+export default App;
